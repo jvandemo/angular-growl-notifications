dab45a1a0112af449996a678c132850dfd070021
Jurgen Van de Moere <jurgen@webberid.com>
2014-01-19 08:16:17 +0100

    Update demo page

--- dist/growl-notifications.js
+++ dist/growl-notifications.js
@@ -58,38 +58,12 @@ angular.module('growlNotifications.directives')
         };
 
     }]);angular.module('growlNotifications.directives')
-    .directive('growlNotifications', ['growlNotifications', '$sce', '$rootScope', function(growlNotifications, $sce, $rootScope){
+    .directive('growlNotifications', ['growlNotifications', '$sce', '$rootScope', '$injector', function(growlNotifications, $sce, $rootScope, $injector){
 
         return {
             restrict: 'AE',
             replace: true,
-            template: '<ul class="list-unstyled"></ul>',
-            link: function(scope, iElem, iAttrs, ctrls, transcludeFn){
-
-                console.log('linked');
-
-                var updateList = function(event, notifications){
-
-                    console.log('HEARD EVENT!');
-                    var items = [];
-
-                    angular.forEach(notifications, function(notification){
-                        var markup = [];
-                        markup.push('<li class="alert alert-' + notification.type + '">');
-                        markup.push($sce.getTrustedHtml(notification.message));
-                        markup.push('</li>');
-                        items.push(markup.join(''));
-                    });
-
-                    iElem.html(items.join(''));
-                };
-
-                scope.$on('growlNotifications::update', updateList);
-
-                updateList(null, growlNotifications.notifications);
-
-            }
-
+            template: '<ul class="list-unstyled"><li ng-repeat="(id, notification) in growlNotifications.notifications"><div class="alert alert-{{notification.type}}"><div ng-bind-html="notification.message"></div></div></li></ul>'
         };
 
     }]);angular.module('growlNotifications.services')
