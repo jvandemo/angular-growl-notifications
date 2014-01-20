ecd50293cc5f02b2e7d5b6d791483ba71dea7f08
Jurgen Van de Moere <jurgen@webberid.com>
2014-01-20 10:57:24 +0100

    Update growlNotifications directive to isolate scope

--- src/growlNotifications/directives/growlNotifications.js
+++ src/growlNotifications/directives/growlNotifications.js
@@ -1,10 +1,14 @@
 angular.module('growlNotifications.directives')
-    .directive('growlNotifications', ['growlNotifications', '$sce', '$rootScope', '$injector', function(growlNotifications, $sce, $rootScope, $injector){
+    .directive('growlNotifications', ['growlNotifications', function(growlNotifications){
 
         return {
             restrict: 'AE',
             replace: true,
-            template: '<ul class="list-unstyled"><li ng-repeat="(id, notification) in growlNotifications.notifications"><div class="alert alert-{{notification.type}}"><div ng-bind-html="notification.message"></div></div></li></ul>'
+            scope: {},
+            template: '<ul class="list-unstyled"><li ng-repeat="(id, notification) in notifications"><div class="alert alert-{{notification.type}}"><div ng-bind-html="notification.message"></div></div></li></ul>',
+            link: function(scope){
+                scope.notifications = growlNotifications.notifications;
+            }
         };
 
     }]);
