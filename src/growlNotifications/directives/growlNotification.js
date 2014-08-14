3ebc52f2029672b9e2ef29e31698648322fcf279
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-08-14 17:18:06 +0200

    feat(growlNotification): refactor compile function to link function

--- src/growlNotifications/directives/growlNotification.js
+++ src/growlNotifications/directives/growlNotification.js
@@ -8,47 +8,33 @@ angular.module('growlNotifications.directives')
     return {
       restrict: 'AE',
       scope: true,
-      compile: function (tElem, tAttrs) {
+      link: function (scope, iElem, iAttrs) {
 
-        if (!growlNotifications.element) {
-          throw new Error('Skipping growlNotification directive because no growlNotifications directive has been defined');
-        }
+        // Assemble options
+        var options = angular.extend({}, defaults, scope.$eval(iAttrs.growlNotification)),
+            timer;
 
-        return {
+        // Move the element to the right location in the DOM
+        $animate.move(iElem, growlNotifications.element);
 
-          // Wait for post link function so all child elements have been compiled
-          // and linked to their correct scope
-          post: function (scope, iElem, iAttrs) {
+        // Provide a remove function to remove the growl instance
+        scope.remove = function () {
 
-            // Assemble options
-            var options = angular.extend({}, defaults, scope.$eval(iAttrs.growlNotification)),
-                timer;
+          // Remove the element
+          $animate.leave(iElem);
 
-            // Move the element to the right location in the DOM
-            $animate.move(iElem, growlNotifications.element);
-
-            // Provide a remove function to remove the growl instance
-            scope.remove = function () {
-
-              // Remove the element
-              $animate.leave(iElem);
-
-              // Cancel scheduled automatic removal if there is one
-              if (timer) {
-                timer.cancel();
-              }
-            };
-
-            // Schedule automatic removal
-            timer = $timeout(function () {
-              $animate.leave(iElem);
-            }, options.ttl);
-
+          // Cancel scheduled automatic removal if there is one
+          if (timer) {
+            timer.cancel();
           }
         };
 
+        // Schedule automatic removal
+        timer = $timeout(function () {
+          $animate.leave(iElem);
+        }, options.ttl);
+
       }
     };
-
 
   }]);
