e3efa28994e0a6213cc569c1cd87587248d8f719
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-08-19 09:42:32 +0200

    feat(examples): add "dynamically-add-notifications" example

--- _harp/public/js/app.js
+++ _harp/public/js/app.js
@@ -1,1 +1,46 @@
-angular.module('examples', ['growlNotifications', 'ngAnimate']);
+angular.module('examples', ['growlNotifications', 'ngAnimate'])
+
+  .controller('AddDynamicNotificationsExampleCtrl', ['$scope', function($scope){
+
+    /**
+     * Initialize index
+     * @type {number}
+     */
+    var index = 0;
+
+    /**
+     * Boolean to show error if new notification is invalid
+     * @type {boolean}
+     */
+    $scope.invalidNotification = false;
+
+    /**
+     * Placeholder for notifications
+     *
+     * We use a hash with auto incrementing key
+     * so we can use "track by" in ng-repeat
+     *
+     * @type {{}}
+     */
+    $scope.notifications = {};
+
+    /**
+     * Add a notification
+     *
+     * @param notification
+     */
+    $scope.add = function(notification){
+
+      var i;
+
+      if(!notification){
+        $scope.invalidNotification = true;
+        return;
+      }
+
+      i = index++;
+      $scope.invalidNotification = false;
+      $scope.notifications[i] = notification;
+    };
+
+  }]);
