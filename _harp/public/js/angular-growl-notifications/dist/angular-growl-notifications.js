02ac153688e9717345437a8e3343d1b3461a4751
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-09-15 22:36:36 +0200

    feat(dist): update build

--- _harp/public/js/angular-growl-notifications/dist/angular-growl-notifications.js
+++ _harp/public/js/angular-growl-notifications/dist/angular-growl-notifications.js
@@ -1,15 +1,12 @@
-(function(window, document) {
-// Create all modules and define dependencies to make sure they exist
-// and are loaded in the correct order to satisfy dependency injection
-// before all nested files are concatenated by Grunt
+(function(){
 
-// Config
+  // Config
   angular.module('growlNotifications.config', [])
     .value('growlNotifications.config', {
       debug: true
     });
 
-// Modules
+  // Modules
   angular.module('growlNotifications.directives', []);
   angular.module('growlNotifications.filters', []);
   angular.module('growlNotifications.services', []);
@@ -20,163 +17,208 @@
       'growlNotifications.filters',
       'growlNotifications.services'
     ]);
-  angular.module('growlNotifications.directives')
-    .directive('growlNotification', ['growlNotifications', '$animate', '$timeout', function (growlNotifications, $animate, $timeout) {
 
-      var defaults = {
-        ttl: growlNotifications.options.ttl || 5000
-      };
+})();(function () {
 
-      return {
+  function growlNotificationDirective(growlNotifications, $animate, $timeout){
 
-        /**
-         * Allow compilation via attributes as well so custom
-         * markup can be used
-         */
-        restrict: 'AE',
+    var defaults = {
+      ttl: growlNotifications.options.ttl || 5000
+    };
 
-        /**
-         * Create new child scope
-         */
-        scope: true,
-
-        /**
-         * Controller
-         *
-         * @param $scope
-         * @param $element
-         * @param $attrs
-         */
-        controller: function($scope, $element, $attrs){
-
-          /**
-           * Placeholder for timer promise
-           */
-          this.timer = null;
-
-          /**
-           * Helper method to close notification manually
-           */
-          this.remove = function(){
-
-            // Remove the element
-            $animate.leave($element);
-
-            // Cancel scheduled automatic removal if there is one
-            if (this.timer && this.timer.cancel) {
-              this.timer.cancel();
-            }
-          };
-
-        },
-
-        /**
-         * Make the controller available in the directive scope
-         */
-        controllerAs: '$growlNotification',
-
-        /**
-         * Post link function
-         *
-         * @param scope
-         * @param iElem
-         * @param iAttrs
-         * @param ctrl
-         */
-        link: function (scope, iElem, iAttrs, ctrl) {
-
-          // Assemble options
-          var options = angular.extend({}, defaults, scope.$eval(iAttrs.growlNotificationOptions));
-
-          if(iAttrs.ttl){
-            options.ttl = scope.$eval(iAttrs.ttl);
-          }
-
-          // Move the element to the right location in the DOM
-          $animate.move(iElem, growlNotifications.element);
-
-          // Schedule automatic removal
-          ctrl.timer = $timeout(function () {
-            $animate.leave(iElem);
-          }, options.ttl);
-
-        }
-      };
-
-    }]);angular.module('growlNotifications.directives')
-    .directive('growlNotifications', ['growlNotifications', function (growlNotifications) {
-
-      return {
-
-        /**
-         * Allow compilation via attributes as well so custom
-         * markup can be used
-         */
-        restrict: 'AE',
-
-        /**
-         * Post link function
-         *
-         * @param scope
-         * @param iElem
-         * @param iAttrs
-         */
-        link: function (scope, iElem, iAttrs) {
-          growlNotifications.element = iElem;
-        }
-      };
-
-    }]);angular.module('growlNotifications.services')
-    .provider('growlNotifications', [function () {
-
-      // Default options
-      var options = {
-        ttl: 5000
-      };
+    return {
 
       /**
-       * Provider method to change default options
-       *
-       * @param newOptions
+       * Allow compilation via attributes as well so custom
+       * markup can be used
        */
-      this.setOptions = function (newOptions) {
-        angular.extend(options, newOptions);
+      restrict: 'AE',
+
+      /**
+       * Create new child scope
+       */
+      scope: true,
+
+      /**
+       * Controller
+       */
+      controller: growlNotificationController,
+
+      /**
+       * Make the controller available in the directive scope
+       */
+      controllerAs: '$growlNotification',
+
+      /**
+       * Post link function
+       *
+       * @param scope
+       * @param iElem
+       * @param iAttrs
+       * @param ctrl
+       */
+      link: function (scope, iElem, iAttrs, ctrl) {
+
+        // Assemble options
+        var options = angular.extend({}, defaults, scope.$eval(iAttrs.growlNotificationOptions));
+
+        if (iAttrs.ttl) {
+          options.ttl = scope.$eval(iAttrs.ttl);
+        }
+
+        // Move the element to the right location in the DOM
+        $animate.move(iElem, growlNotifications.element);
+
+        // Schedule automatic removal
+        ctrl.timer = $timeout(function () {
+          $animate.leave(iElem);
+        }, options.ttl);
+
+      }
+    };
+
+  }
+
+  // Inject dependencies
+  growlNotificationDirective.$inject = ['growlNotifications', '$animate', '$timeout'];
+
+  /**
+   * Directive controller
+   *
+   * @param $scope
+   * @param $element
+   */
+  function growlNotificationController($element, $animate) {
+
+    /**
+     * Placeholder for timer promise
+     */
+    this.timer = null;
+
+    /**
+     * Helper method to close notification manually
+     */
+    this.remove = function () {
+
+      // Remove the element
+      $animate.leave($element);
+
+      // Cancel scheduled automatic removal if there is one
+      if (this.timer && this.timer.cancel) {
+        this.timer.cancel();
+      }
+    };
+
+  }
+
+  // Inject dependencies
+  growlNotificationController.$inject = ['$element', '$animate'];
+
+  // Export
+  angular
+    .module('growlNotifications.directives')
+    .directive('growlNotification', growlNotificationDirective);
+
+})();(function () {
+
+  /**
+   * Create directive definition object
+   *
+   * @param growlNotifications
+   */
+  function growlNotificationsDirective(growlNotifications) {
+
+    return {
+
+      /**
+       * Allow compilation via attributes as well so custom
+       * markup can be used
+       */
+      restrict: 'AE',
+
+      /**
+       * Post link function
+       *
+       * @param scope
+       * @param iElem
+       * @param iAttrs
+       */
+      link: function (scope, iElem, iAttrs) {
+        growlNotifications.element = iElem;
+      }
+    };
+
+  }
+
+  // Inject dependencies
+  growlNotificationsDirective.$inject = ['growlNotifications'];
+
+  // Export
+  angular
+    .module('growlNotifications.directives')
+    .directive('growlNotifications', growlNotificationsDirective);
+
+})();(function () {
+
+  /**
+   * Growl notifications provider
+   */
+  function growlNotificationsProvider(){
+
+    // Default options
+    var options = {
+      ttl: 5000
+    };
+
+    /**
+     * Provider method to change default options
+     *
+     * @param newOptions
+     */
+    this.setOptions = function (newOptions) {
+      angular.extend(options, newOptions);
+      return this;
+    };
+
+    /**
+     * Provider convenience method to get or set default ttl
+     *
+     * @param ttl
+     * @returns {*}
+     */
+    this.ttl = function (ttl) {
+      if (angular.isDefined(ttl)) {
+        options.ttl = ttl;
         return this;
-      };
+      }
+      return options.ttl;
+    };
 
-      /**
-       * Provider convenience method to get or set default ttl
-       *
-       * @param ttl
-       * @returns {*}
-       */
-      this.ttl = function(ttl){
-        if(angular.isDefined(ttl)){
-          options.ttl = ttl;
-          return this;
-        }
-        return options.ttl;
-      };
+    /**
+     * Factory method
+     *
+     * @param $timeout
+     * @param $rootScope
+     * @returns {GrowlNotifications}
+     */
+    this.$get = function () {
 
-      /**
-       * Factory method
-       *
-       * @param $timeout
-       * @param $rootScope
-       * @returns {GrowlNotifications}
-       */
-      this.$get = function () {
+      function GrowlNotifications() {
 
-        function GrowlNotifications() {
+        this.options = options;
+        this.element = null;
 
-          this.options = options;
-          this.element = null;
+      }
 
-        }
+      return new GrowlNotifications();
 
-        return new GrowlNotifications();
+    };
 
-      };
+  }
 
-    }]);
-})(window, document);
+  // Export
+  angular
+    .module('growlNotifications.services')
+    .provider('growlNotifications', growlNotificationsProvider);
+
+})();
